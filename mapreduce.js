5d3c1f2020b5d5e4a9846bd8b78d291f1b9b2d18
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-04-21 18:29:54 +0530

    Update mapreduce.js

--- mapreduce.js
+++ mapreduce.js
@@ -7,9 +7,21 @@ let a1=arr.map((value,index) => {
   return value+1
 })
 console.log(a1)
+
+
 //array filter method
 let arr2= [2,4,50]
 let a2=arr2.filter((value)=>{
   return value<10
 })
 console.log(a2)
+
+
+
+//array reduce 
+let arr3=[2,3,4,5,4]
+let a3=arr3.reduce((h1, h2)=>{
+  return h1+h2
+})
+console.log(a3)
+
